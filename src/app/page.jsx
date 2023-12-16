e3d1f619b549f7c5ff13b71bf60d658dde3aa8f6
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2023-12-16 15:16:54 +0200

    Test Version

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import FollowUsSection from "../sections/FollowUsSection/FollowUsSection"
 import AppSection from "../sections/AppSection/AppSection"
 import AboutSection from "../sections/AboutSection/AboutSection"
 import MainSection from "../sections/MainSection/MainSection"
+import TestmonialSection from "../sections/TestmonialSection/TestmonialSection"
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <MainSection />
       <AboutSection />
       <AppSection />
+      <TestmonialSection />
       <FollowUsSection />
       <ContactSection />
     </>
--- src/sections/TestmonialSection/TestmonialSection.jsx
+++ src/sections/TestmonialSection/TestmonialSection.jsx
@@ -1,7 +1,47 @@
-
+"use client"
+import { LazyLoadImage } from "react-lazy-load-image-component"
+import { PrimaryBox } from "../../muiCustomize/PrimaryBox"
+import { PrimaryContainer } from "../../muiCustomize/PrimaryContainer"
+import { Box, Typography } from "@mui/material"
+import styles from "./TestmonialSection.module.scss"
+import { useTranslation } from "react-i18next"
+import quoteImg from "../../assets/images/quotes.svg"
+import backgroundImg from "../../assets/images/background.svg"
+import starsImg from "../../assets/images/stars.svg"
+import clients from "../../data/clients-review.json";
+const reviews = clients.cards
 const TestmonialSection = () => {
+  const { t } = useTranslation()
   return (
-    <div>TestmonialSection</div>
+    <PrimaryBox>
+      <PrimaryContainer className={`grid jcs aic g50 ${styles.testmonial_contain}`}>
+        <Typography variant="h4" className={`tac fw700`}>{t("testmonail.title")}</Typography>
+        <Box className={`grid jcs aic g30 ${styles.reviews}`}>
+          {reviews.map((review, i) => (
+            <Box className={`grid jcs aic g20 pad20 br6 ${styles.review}`}>
+              <Box className={`flex jcc aic`}>
+                <LazyLoadImage src={quoteImg.src} alt={"quote"} />
+              </Box>
+              <Typography variant="h5">{t("lang") === "ar" ? review.arDescription : review.enDescription}</Typography>
+              <Box className={`flex jcsb g20`}>
+                <Box className={`flex jcs aic`}>
+                  <Box className={`flex jcc aic`}>
+                    <LazyLoadImage src={backgroundImg.src} alt={"user"} />
+                  </Box>
+                  <Box className={`grid jcs aic g5`}>
+                    <Typography variant="h5">{t("lang") === "ar" ? review.arClientname : review.enClientname}</Typography>
+                    <Typography variant="h6" sx={{ color: (theme) => theme.palette.gray }}>{t("lang") === "ar" ? review.arRole : review.enRole}</Typography>
+                  </Box>
+                </Box>
+                <Box className={`flex jcfe aic`}>
+                  <LazyLoadImage src={starsImg.src} alt={"stars"} />
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Box>
+      </PrimaryContainer>
+    </PrimaryBox>
   )
 }
 
